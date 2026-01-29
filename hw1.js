1c9f3dd207134050997cc5f3b7ab3e4098e63350
HenryC0416 <henrychen0416@gmail.com>
2026-01-28 23:06:15 -0500

    Added summation and averaging function

---
+++ hw1.js
@@ -0,0 +1,25 @@
+//Array Functions
+
+
+//
+function sum(arr){
+    let total=0;
+    for (const n of arr){
+        total+=n;
+    }
+    return total
+}
+
+function average(arr){
+    return sum(arr)/arr.length;
+}
+
+
+
+//String Functions
+
+
+
+//Object Practice
+
+
